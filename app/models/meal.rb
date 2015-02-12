f7136b2b5b941f6fd99e7fdead489e0967412adc
Alexey Shein <alexey.shein.dev@gmail.com>
2015-02-13 02:18:34 +0500

    Add check for meal calories should be greater than zero.

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -3,5 +3,6 @@ class Meal < ActiveRecord::Base
   belongs_to :user
 
   validates :details, :calories, :created_at, presence: true
+  validates :calories, numericality: { greater_than: 0 }
 
 end
--- spec/models/meal_spec.rb
+++ spec/models/meal_spec.rb
@@ -7,4 +7,6 @@ RSpec.describe Meal, type: :model do
   it { should validate_presence_of(:created_at) }
 
   it { should belong_to(:user) }
+
+  it { should validate_numericality_of(:calories).is_greater_than(0) }
 end
